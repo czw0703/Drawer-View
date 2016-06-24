6d7d2f8ba743ba67aaccbb14d289f11d456b654b
mac <mac@vae.local>
2016-06-24 19:32:07 +0800

    简单的抽屉视图计算

--- DrawerView/DrawerView/MainViewController.h
+++ DrawerView/DrawerView/MainViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface MainViewController : UIViewController
-
+@property (nonatomic, weak, readonly) UIView *mainV;
+@property (nonatomic, weak, readonly) UIView *leftV;
+@property (nonatomic, weak, readonly) UIView *rightV;
 @end
